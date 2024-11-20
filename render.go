9e6c31c323271bebf35bdac306adf94310cd33da
replica <60418809+junobonnie@users.noreply.github.com>
2024-11-20 23:07:49 +0900

    commit

--- render.go
+++ render.go
@@ -14,7 +14,7 @@ func Rendering(width float64, height float64, depth float64, filename string, di
 	dc.DrawRectangle(0, 0, w, h)
 	dc.Fill()
 
-	_, t, count, N, _, pos, _ := Read(filename)
+	_, _, count, N, _, pos, _ := Read(filename)
 
 	for i := 0; i < int(N); i++ {
 		dc.DrawCircle(10.*pos[i].X, h-10*pos[i].Y, 5)
@@ -32,5 +32,4 @@ func Rendering(width float64, height float64, depth float64, filename string, di
 		os.Mkdir(directory, os.ModeDir|0755)
 	}
 	dc.SavePNG(fmt.Sprintf("%s/render_%010d.png", directory, count))
-	fmt.Println(int(t), "is done")
 }
